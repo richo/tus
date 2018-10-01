906643d0a518d1985461ac408c878bea724d2f6c
Richo Healey <richo@psych0tik.net>
2018-10-01 12:53:14 -0700

    Actually write some tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,19 +29,19 @@ lazy_static! {
 }
 
 /// A client for a TUS endpoint. This leaks a lot of the implementation details of reqwest.
-pub struct Client {
-    url: String,
+pub struct Client<'a> {
+    url: &'a str,
     headers: HeaderMap,
     // TODO(richo) Make this generic over some trait so we can test it
     client: reqwest::Client,
 }
 
-impl Client {
+impl<'a> Client<'a> {
     /// Creates a new Client.
     ///
     /// Headers should be a HeaderMap preloaded with all necessary information to communicate with
     /// the endpoint, including eg authentication information.
-    pub fn new(url: String, headers: HeaderMap) -> Client {
+    pub fn new(url: &'a str, headers: HeaderMap) -> Client {
         Client {
             url,
             headers,
@@ -50,25 +50,36 @@ impl Client {
     }
 
     /// Uploads all content from `reader` to the endpoint, consuming this Client.
-    pub fn upload<T>(mut self, mut reader: T) -> Result<usize, Error>
+    pub fn upload<T>(self, reader: T) -> Result<usize, Error>
     where T: Read {
-        let mut offset = 0;
-        loop {
-            let mut buf = Vec::with_capacity(CHUNK_SIZE);
-            let len = reader.read(&mut buf)?;
-            if len == 0 {
-                return Ok(offset)
-            }
+        self.upload_inner(reader, |chunk, offset| {
             let mut headers = self.headers.clone();
             headers.insert(UPLOAD_OFFSET.clone(), HeaderValue::from_str(&format!("{}", offset))?);
-            offset += self.upload_chunk(buf, headers)?;
+            self.upload_chunk(chunk, headers)
+        })
+    }
+
+    fn upload_inner<T, U>(&self, mut reader: T, mut cb: U) -> Result<usize, Error>
+    where T: Read,
+          U: FnMut(Vec<u8>, usize) -> Result<usize, Error>,
+    {
+        let mut offset = 0;
+        loop {
+            let mut chunk = vec![0; CHUNK_SIZE];
+            let bytes_read = reader.read(&mut chunk)?;
+            if bytes_read == 0 {
+                return Ok(offset)
+            }
+            chunk.truncate(bytes_read);
+            cb(chunk, bytes_read)?;
+            offset += bytes_read;
         }
     }
 
-    fn upload_chunk(&mut self, chunk: Vec<u8>, headers: HeaderMap) -> Result<usize, Error> {
+    fn upload_chunk(&self, chunk: Vec<u8>, headers: HeaderMap) -> Result<usize, Error> {
         let len = chunk.len();
         let res = self.client
-            .patch(&self.url)
+            .patch(self.url)
             .body(chunk)
             .headers(headers)
             .send()?;
@@ -83,8 +94,58 @@ impl Client {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::fs::File;
+    use std::io::{Seek, Write};
+    extern crate rand;
+    extern crate tempfile;
+
+    /// Create a dummy client for use in tests
+    fn test_client<'a>() -> Client<'a> {
+        Client {
+            url: "https://test-url.com/foo",
+            headers: HeaderMap::new(),
+            client: reqwest::Client::new(),
+        }
+    }
+
+    /// Creates a file filled with entropy, and returns it along with the entropy used.
+    ///
+    /// The buffer is intentionally not a very even size, to ensure that tests that verify chunking
+    /// at 1k boundaries do the right thing.
+    fn entropy_filled_file<'a>() -> (File, Vec<u8>) {
+        use tests::rand::prelude::*;
+        let mut tmp = tempfile::tempfile().expect("Couldn't create tempfile");
+        let mut rng = thread_rng();
+
+        let mut bytes = vec![0; 1024 * 1024 * 12 + 768];
+        rng.fill(&mut bytes[..]);
+        let written = tmp.write(&mut bytes).expect("Couldn't fill buffer");
+        assert!(written > 0, "Didn't write anything to the tempfile");
+        tmp.seek(std::io::SeekFrom::Start(0));
+
+        (tmp, bytes)
+    }
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_chunking_works() {
+        let (file, bytes) = entropy_filled_file();
+        let client = test_client();
+
+        let mut vec: Vec<u8> = vec![];
+        client.upload_inner(file, |chunk, offset| {
+            vec.extend(&chunk);
+            Ok(chunk.len())
+        });
+
+        assert_eq!(&vec[..], &bytes[..]);
+    }
+
+    #[test]
+    fn test_entropy_works() {
+        let (mut file, bytes) = entropy_filled_file();
+        let mut vec = Vec::with_capacity(1024);
+        file.read_to_end(&mut vec);
+        assert_eq!(&bytes[..], &vec[..]);
     }
 }
